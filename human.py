77d6e96fd7a4657d309ec4cf036260cc49814397
mrtong96 <mrtong96@berkeley.edu>
2015-08-30 22:37:20 -0700

    added shortcuts for moving rps

--- human.py
+++ human.py
@@ -7,12 +7,17 @@ class Human():
 
     # gamestate variable not used, for compatibility with CPU method
     def choose_move(self, gamestate):
-        valid_responses = ['r', 'p', 's', 'rock', 'paper', 'scissors']
+        valid_responses = ['r', 'p', 's', 'q', 'w', 'e', 'rock', 'paper', 'scissors']
         while True:
             response = raw_input('choose a move\n')
             if response in valid_responses:
                 break
             print ('error: need a valid response, valid responses: {}'.format(valid_responses))
+            print ('q=rock, w=paper, e=scissors')
+        shortcuts = ['q', 'w', 'e']
+        shortcut_key = {'q': 'r', 'w': 'p', 'e': 's'}
+        if response in shortcuts:
+            response = shortcut_key[response]
         return response[0]
 
     def process_moves(self, gamestate):
